73c21374f14511d433b0fc41615cdfb3bbfadecf
Akihiko Kuroda <akihikokuroda2020@gmail.com>
2025-07-14 20:36:22 -0400

    fix cli docker file
    Signed-off-by: Akihiko Kuroda <akihikokuroda2020@gmail.com>

--- tools/buildimg.sh
+++ tools/buildimg.sh
@@ -12,5 +12,5 @@ echo $VERSION
 uv build
 
 # build container images
-$CONTAINER_CMD build -t ghcr.io/$GITHUB_REPOSITORY/maestro:$VERSION -f Dockerfile --build-arg MAESTRO_VERSION=$VERSION .
-$CONTAINER_CMD build -t ghcr.io/$GITHUB_REPOSITORY/maestro-cli:$VERSION -f Dockerfile-cli --build-arg MAESTRO_VERSION=$VERSION .
+$CONTAINER_CMD build -t ghcr.io/$GITHUB_REPOSITORY/maestro:$VERSION -f Dockerfile --build-arg MAESTRO_VERSION=$VERSION --build-arg MAESTRO_REPOSITORY=$GITHUB_REPOSITORY .
+$CONTAINER_CMD build -t ghcr.io/$GITHUB_REPOSITORY/maestro-cli:$VERSION -f Dockerfile-cli --build-arg MAESTRO_VERSION=$VERSION --build-arg MAESTRO_REPOSITORY=$GITHUB_REPOSITORY .
