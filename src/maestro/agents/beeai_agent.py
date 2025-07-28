AI4quantum/maestro

commit 6af2ccf42210cfc99abcb8f4a1f7d0fb1396f549

feat: enable llm url override via yaml spec (#664)
Signed-off-by: Alex Bozarth <ajbozart@us.ibm.com>

src/maestro/agents/beeai_agent.py
@@ -7,7 +7,9 @@ import tempfile
 import requests
 import json
 
+from beeai_framework.adapters.ollama import OllamaChatModel
 from beeai_framework.agents.tool_calling import ToolCallingAgent
+from beeai_framework.backend import ChatModel
 from beeai_framework.tools.code import PythonTool, LocalPythonStorage, SandboxTool
 from openai import AssistantEventHandler, OpenAI
 from openai.types.beta import AssistantStreamEvent
@@ -16,7 +18,6 @@ from openai.types.beta.threads.runs import RunStep, RunStepDelta, ToolCall
 from typing import Any, Callable
 from pydantic import BaseModel
 
-from beeai_framework.adapters.ollama import OllamaChatModel
 from beeai_framework.agents import AgentExecutionConfig, AgentMeta
 from beeai_framework.emitter import Emitter, EmitterOptions, EventMeta
 from beeai_framework.errors import FrameworkError
@@ -46,7 +47,9 @@ class BeeAIAgent(Agent):
         """
         super().__init__(agent)
 
-        url = f"{os.getenv('BEE_API')}/v1/assistants"
+        self.base_url = f"{agent['spec'].get('url', os.getenv('BEE_API'))}/v1"
+
+        url = f"{self.base_url}/assistants"
         headers = {
             "accept": "application/json",
             "Authorization": "Bearer sk-proj-testkey",
@@ -85,7 +88,8 @@ class BeeAIAgent(Agent):
         """
         self.print(f"Running {self.agent_name}...\n")
         client = OpenAI(
-            base_url=f"{os.getenv('BEE_API')}/v1", api_key=os.getenv("BEE_API_KEY")
+            base_url=f"{self.base_url}/v1",
+            api_key=os.getenv("BEE_API_KEY", "dummy_key"),
         )
         # TODO: Unused currently
         # assistant = client.beta.assistants.retrieve(self.agent_id)
@@ -108,7 +112,8 @@ class BeeAIAgent(Agent):
         """
         self.print(f"Running {self.agent_name}...\n")
         client = OpenAI(
-            base_url=f"{os.getenv('BEE_API')}/v1", api_key=os.getenv("BEE_API_KEY")
+            base_url=f"{self.base_url}/v1",
+            api_key=os.getenv("BEE_API_KEY", "dummy_key"),
         )
         # TODO: Unused currently
         # assistant = client.beta.assistants.retrieve(self.agent_id)
@@ -241,7 +246,14 @@ class BeeAILocalAgent(Agent):
         self.agent = None
 
     async def _create_agent(self):
-        llm = OllamaChatModel(self.agent_model)
+        if len(self.agent_model.split("/")) < 2:
+            llm = OllamaChatModel(
+                self.agent_model, base_url=self.agent["spec"].get("url")
+            )
+        else:
+            llm = ChatModel.from_name(
+                self.agent_model, base_url=self.agent["spec"].get("url")
+            )
 
         templates: dict[str, Any] = {
             "user": user_template_func,

src/maestro/agents/openai_agent.py
@@ -54,8 +54,10 @@ class OpenAIAgent(MaestroAgent):
 
         spec_dict = agent_definition.get("spec", {})
         self.model_name: str = spec_dict.get("model", OPENAI_DEFAULT_MODEL)
-        self.base_url: str = os.getenv("OPENAI_BASE_URL", OPENAI_DEFAULT_URL)
-        self.api_key: Optional[str] = os.getenv("OPENAI_API_KEY")
+        self.base_url: str = spec_dict.get(
+            "url", os.getenv("OPENAI_BASE_URL", OPENAI_DEFAULT_URL)
+        )
+        self.api_key: Optional[str] = os.getenv("OPENAI_API_KEY", "dummy_key")
         self.uses_chat_completions: bool = self.base_url != OPENAI_DEFAULT_URL
         self.use_litellm: bool = (
             os.getenv("MAESTRO_OPENAI_USE_LITELLM", "false").lower() == "true"

tests/yamls/agents/simple_local_agent.yaml
@@ -6,7 +6,7 @@ metadata:
     app: test-example
 spec:
 
-  model: llama3.1
+  model: ollama/llama3.1
   framework: beeai
   mode: local
   description: this is a test
@@ -24,7 +24,7 @@ metadata:
   labels:
     app: test-example
 spec:
-  model: llama3.1
+  model: ollama/llama3.1
   mode: local
   framework: beeai
   description: this is a test
@@ -42,7 +42,7 @@ metadata:
   labels:
     app: test-example
 spec:
-  model: llama3.1
+  model: ollama/llama3.1
   framework: beeai
   mode: local
   description: this is a test
@@ -60,7 +60,7 @@ metadata:
   labels:
     app: test-example
 spec:
-  model: llama3.1
+  model: ollama/llama3.1
   framework: beeai
   mode: local
   description: this is a test
@@ -78,7 +78,7 @@ metadata:
   labels:
     app: test-example
 spec:
-  model: llama3.1
+  model: ollama/llama3.1
   framework: beeai
   mode: local
   description: this is a test

commit c2f14b6f0ce6ee50b8ae558d1231a880abe062ac

fix: beeai agent broken from #664
Signed-off-by: Alex Bozarth <ajbozart@us.ibm.com>

src/maestro/agents/beeai_agent.py
@@ -10,6 +10,7 @@ import json
 from beeai_framework.adapters.ollama import OllamaChatModel
 from beeai_framework.agents.tool_calling import ToolCallingAgent
 from beeai_framework.backend import ChatModel
+from beeai_framework.backend.utils import find_provider_def
 from beeai_framework.tools.code import PythonTool, LocalPythonStorage, SandboxTool
 from openai import AssistantEventHandler, OpenAI
 from openai.types.beta import AssistantStreamEvent
@@ -88,8 +89,8 @@ class BeeAIAgent(Agent):
         """
         self.print(f"Running {self.agent_name}...\n")
         client = OpenAI(
-            base_url=f"{self.base_url}/v1",
-            api_key=os.getenv("BEE_API_KEY", "dummy_key"),
+            base_url=self.base_url,
+            api_key=os.getenv("BEE_API_KEY", "sk-proj-testkey"),
         )
         # TODO: Unused currently
         # assistant = client.beta.assistants.retrieve(self.agent_id)
@@ -112,8 +113,8 @@ class BeeAIAgent(Agent):
         """
         self.print(f"Running {self.agent_name}...\n")
         client = OpenAI(
-            base_url=f"{self.base_url}/v1",
-            api_key=os.getenv("BEE_API_KEY", "dummy_key"),
+            base_url=self.base_url,
+            api_key=os.getenv("BEE_API_KEY", "sk-proj-testkey"),
         )
         # TODO: Unused currently
         # assistant = client.beta.assistants.retrieve(self.agent_id)
@@ -246,10 +247,10 @@ class BeeAILocalAgent(Agent):
         self.agent = None
 
     async def _create_agent(self):
-        if len(self.agent_model.split("/")) < 2:
-            llm = OllamaChatModel(self.agent_model, base_url=self.agent_url)
-        else:
+        if find_provider_def(self.agent_model.split(":")[0]) is not None:
             llm = ChatModel.from_name(self.agent_model, base_url=self.agent_url)
+        else:
+            llm = OllamaChatModel(self.agent_model, base_url=self.agent_url)
 
         templates: dict[str, Any] = {
             "user": user_template_func,