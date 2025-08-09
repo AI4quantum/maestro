c3c2565252fdf19b246e3fce40395fd036c219b2
Akihiko Kuroda <akihikokuroda2020@gmail.com>
2025-08-09 12:58:27 -0400

    add maestro create support
    Signed-off-by: Akihiko Kuroda <akihikokuroda2020@gmail.com>

--- src/maestro/mcptool.py
+++ src/maestro/mcptool.py
@@ -14,6 +14,15 @@ group = "toolhive.stacklok.dev"
 version = "v1alpha1"
 kind = "MCPServer"
 
+# Define the plural and singular names for the custom resource
+remotePlural = "remotemcpservers"
+remoteSingular = "remotemcpserver"
+
+# Define the group, version, and kind for the custom resource
+remoteGroup = "maestro.ai4quantum.com"
+remoteVersion = "v1alpha1"
+remoteKind = "RemoteMCPServer"
+
 
 def create_mcptools(tool_defs):
     # Load kubeconfig
@@ -25,13 +34,27 @@ def create_mcptools(tool_defs):
 def create_mcptool(body):
     # Create an instance of the API class for the custom resource definition
     api_instance = client.CustomObjectsApi()
-    # Create the CRD instance
-    body["apiVersion"] = f"{group}/{version}"
-    body["kind"] = "MCPServer"
-    namespace = body["metadata"].get("namespace")
-    if not namespace:
-        namespace = "default"
-    api_response = api_instance.create_namespaced_custom_object(
-        group, version, namespace, plural, body
-    )
-    print(f"MCP tool: {api_response['metadata']['name']} successfully created")
+    url = body["spec"].get("url")
+    if url:
+        body["apiVersion"] = f"{remoteGroup}/{remoteVersion}"
+        body["kind"] = "RemoteMCPServer"
+        namespace = body["metadata"].get("namespace")
+        if not namespace:
+            namespace = "default"
+        api_response = api_instance.create_namespaced_custom_object(
+            remoteGroup, remoteVersion, namespace, remotePlural, body
+        )
+        print(
+            f"Remote MCP tool: {api_response['metadata']['name']} successfully created"
+        )
+    else:
+        # Create the CRD instance
+        body["apiVersion"] = f"{group}/{version}"
+        body["kind"] = "MCPServer"
+        namespace = body["metadata"].get("namespace")
+        if not namespace:
+            namespace = "default"
+        api_response = api_instance.create_namespaced_custom_object(
+            group, version, namespace, plural, body
+        )
+        print(f"MCP tool: {api_response['metadata']['name']} successfully created")
